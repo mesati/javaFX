590dcb0452680545d43be1e968470949659ab524
WINDOWS 10 <WINDOWS 10@WINDOWS>
2018-09-04 17:09:20 +0700

    commit javaFX

---
+++ src/latihanjavafx/FXMLKalkulatorSederhanaController.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjavafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+/**
+ * FXML Controller class
+ *
+ * @author WINDOWS 10
+ */
+public class FXMLKalkulatorSederhanaController implements Initializable {
+
+    @FXML
+    private TextField bil1;
+
+    @FXML
+    private TextField bil2;
+
+    @FXML
+    private JFXButton tambah;
+
+    @FXML
+    private JFXButton kurang;
+
+    @FXML
+    private JFXButton kali;
+
+    @FXML
+    private JFXButton bagi;
+
+    @FXML
+    private JFXTextField hasil;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+     @FXML
+    void pembagian(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pengurangan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void perkalian(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pertambahan(ActionEvent event) {
+
+    }
+
+}
+
